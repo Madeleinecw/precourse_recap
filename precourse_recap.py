650c3b2588a09ee7da530dc212346d940b2caaf4
Madeleine Wood <madeleinecwood@gmail.com>
2020-08-31 16:32:08 +0100

    first commit

---
+++ precourse_recap.py
@@ -0,0 +1,7 @@
+my_plant = input("What is my favourite plant? ")
+if my_plant == "spider":
+    print("spider plants are ok")
+elif my_plant == "monstera":
+    print("monstera are my favourite plants")
+else:
+    print("that is not my favourite plant")
